8436c0b774c77014e40e3a3f58602ef4248ac40e
amrmosleh <amrmosleh@yahoo.com>
2022-05-27 15:47:04 -0400

    commented test 3

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ def test2():
     response = app.test_client().get('/edit')
     assert response.status_code == 200
 
-def test3():
-    """A dummy docstring"""
-    response = app.test_client().get("/edit")
-    assert b"To-Do Application" in response.data
-    assert b"Todo Title" in response.data
-    assert b"Add" in response.data
+# def test3():
+#     """A dummy docstring"""
+#     response = app.test_client().get("/edit")
+#     assert b"To-Do Application" in response.data
+#     assert b"Todo Title" in response.data
+#     assert b"Add" in response.data
