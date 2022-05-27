d97a6efabaaccfd8ab113c4e32ecadfde189f596
amrmosleh <amrmosleh@yahoo.com>
2022-05-27 13:40:58 -0400

    .

---
+++ templates/test.py
@@ -0,0 +1,26 @@
+from app import app
+
+def test1():
+    # """This fuction test the glas applictio has a correct response  code when the application goes live
+    
+
+
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+
+
+
+def test2():
+    response = app.test_client().get('/edit')
+    assert response.status_code == 200
+
+def test3():
+    """A dummy docstring."""
+    response = app.test_client().get("/edit")
+    assert b"To-Do Application" in response.data
+    assert b"Todo Title" in response.data
+    assert b"Add" in response.data
+
+
+def test3():
+    pass
