54b97e4f270047e93f5bd3f56de554159ae75e9c
amrmosleh <amrmosleh@yahoo.com>
2022-05-27 15:54:28 -0400

    replaced all files

--- app.py
+++ app.py
@@ -1,84 +1,51 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-# importing the necessary libarires, compnents, etc, to make the code run
-# - flask
-# -SQLALchemy
-
 '''
-__authors__ = ['Amr Mohamad - amrmosleh]
-__date__ = 2022026
-__description__ = "TO-DO application for CICD"
-
+__authors__ = ['Mostafa Wahied - mostafa-wahied']
+__date__ = 5/26/2022
+__description__ = "Job tracking ToDo list"
 '''
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 class Todo(db.Model):
     """A dummy docstring."""
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
-
-
 @app.route("/")
 def list1():
-    """A dummy docstring."""
+    """A docstring."""
     todo_list = Todo.query.all()
     return render_template("list.html", todo_list=todo_list)
-
-
 @app.route("/edit")
 def home():
-    """A dummy docstring."""
+    """A docstring."""
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
-
-
 @app.route("/add", methods=["POST"])
 def add():
-    """A dummy docstring."""
+    """A docstring."""
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
-
-
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
-    """A dummy docstring."""
+    """A docstring."""
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
-
-
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
-    """A dummy docstring."""
+    """A docstring."""
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("home"))
-
-
-
-if "__name__ == __main__":
+if __name__ == "__main__":
     db.create_all()
     app.run()
-
-
-
-
-
-
-
-
-
-
-
-
