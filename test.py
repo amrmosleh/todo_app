9962471e16b7c21076bc0752ecac46513d81a346
amrmosleh <amrmosleh@yahoo.com>
2022-05-27 15:50:37 -0400

    replaced

--- test.py
+++ test.py
@@ -1,16 +1,19 @@
-from app import app 
-
+from app import app
 def test1():
+    """
+    this function tests that the flask app has a correct response code when the application goes live.
+    """
     response = app.test_client().get('/')
     assert response.status_code == 200
-
 def test2():
+    """
+    this function tests that the flask app has a correct response code when the application goes live.
+    """
     response = app.test_client().get('/edit')
     assert response.status_code == 200
-
-# def test3():
-#     """A dummy docstring"""
-#     response = app.test_client().get("/edit")
-#     assert b"To-Do Application" in response.data
-#     assert b"Todo Title" in response.data
-#     assert b"Add" in response.data
+def test3():
+    """A dummy docstring."""
+    response = app.test_client().get("/edit")
+    assert b"To-Do Application" in response.data
+    assert b"Todo Title" in response.data
+    assert b"Add" in response.data
